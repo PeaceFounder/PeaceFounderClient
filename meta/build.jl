9c2f975e60b0e28e509e8d4ee2e422cf7bd5633f
Janis Erdmanis <janiserdmanis@protonmail.ch>
2024-07-03 14:48:56 +0300

    relocability fix

--- meta/build.jl
+++ meta/build.jl
@@ -10,11 +10,10 @@ mkpath(BUILD_DIR)
 
 VERSION = TOML.parsefile("$APP_DIR/Project.toml")["version"]
 
-# AppBundler.bundle_app(MacOS(:x86_64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-x64.app")
+AppBundler.bundle_app(MacOS(:x86_64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-x64.app")
 AppBundler.bundle_app(MacOS(:aarch64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-arm64.app")
 
-# AppBundler.bundle_app(Linux(:x86_64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-x64.snap")
-# AppBundler.bundle_app(Linux(:aarch64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-arm64.snap")
+AppBundler.bundle_app(Linux(:x86_64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-x64.snap")
+AppBundler.bundle_app(Linux(:aarch64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-arm64.snap")
 
-# AppBundler.bundle_app(Windows(:x86_64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-x64.zip"; path_length_threshold = 180, skip_long_paths = true, debug = true)
-#AppBundler.bundle_app(Windows(:x86_64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-x64.zip"; path_length_threshold = 180, skip_long_paths = true)
+AppBundler.bundle_app(Windows(:x86_64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-x64.zip"; path_length_threshold = 180, skip_long_paths = true)
