2f20d8e612eb83d7704ddd518fe7417235515b5a
Janis Erdmanis <janiserdmanis@protonmail.ch>
2025-06-12 16:29:53 +0300

    continious deployment fixes

--- meta/build.jl
+++ meta/build.jl
@@ -10,12 +10,14 @@ mkpath(BUILD_DIR)
 
 VERSION = TOML.parsefile("$APP_DIR/Project.toml")["version"]
 
-#AppBundler.bundle_app(MacOS(:x86_64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-x64.dmg")
-AppBundler.build_app(MacOS(:aarch64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-arm64.dmg"; incremental=false)
+#AppBundler.build_app(MacOS(:x86_64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-x64.dmg")
+#AppBundler.build_app(MacOS(:aarch64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-arm64.dmg"; incremental=false)
 
 #AppBundler.build_app(MacOS(:aarch64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-arm64.app"; incremental=true)
 
-#AppBundler.bundle_app(Linux(:x86_64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-x64.snap")
-#AppBundler.bundle_app(Linux(:aarch64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-arm64.snap")
+#AppBundler.build_app(Linux(:x86_64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-x64.snap")
+#AppBundler.build_app(Linux(:aarch64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-arm64"; incremental=true, precompile=false)
 
-#AppBundler.bundle_app(Windows(:x86_64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-x64-win"; path_length_threshold = 180, skip_long_paths = true)
+#AppBundler.build_app(Windows(:x86_64), APP_DIR, "$BUILD_DIR/peacefounder-$VERSION-x64-win"; path_length_threshold = 180, skip_long_paths = true)
+
+AppBundler.build_app(Windows(:x86_64), APP_DIR, joinpath(homedir(), "Documents", "peacefounder-$VERSION-x64-win.msix"); path_length_threshold = 180, skip_long_paths = true, precompile=true)
